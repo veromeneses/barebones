620e3841615d44a4210cbce0d7fee4b7aa2b98db
Veronica Meneses <me+git@veromenes.es>
2016-07-07 00:04:32 +0100

    Removed comments.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,5 +1,4 @@
 After do |scenario|
-  # take_screenshot(scenario) if scenario.failed?
   if scenario.failed?
     Dir.mkdir('failure_images') unless File.directory? 'failure_images'
     image = "./failure_images/#{scenario.name}-#{Time.now}"
@@ -8,10 +7,3 @@ After do |scenario|
     embed(image, 'image/png')
   end
 end
-
-# def take_screenshot(scenario)
-#   Dir.mkdir('failure_images') unless File.directory? 'failure_images'
-#   image = "./failure_images/#{scenario.name}-#{Time.now}"
-#   @browser.screenshot.save(image)
-#   embed(image, 'image/png')
-# end
