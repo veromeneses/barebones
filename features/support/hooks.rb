29b7d68022202da7fb01c89ad70349239bb9b0ff
Veronica Meneses <me+git@veromenes.es>
2016-07-07 22:50:09 +0100

    Simplified the screenshot name.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,8 +1,7 @@
 After do |scenario|
   if scenario.failed?
     Dir.mkdir('failure_images') unless File.directory? 'failure_images'
-    image = "./failure_images/#{scenario.name}-#{Time.now.utc}"
-    image = image.downcase.tr(' ','_')
+    image = ("./failure_images/#{scenario.name}-#{Time.now.utc}.png").downcase.tr(' ','_')
     @browser.screenshot.save(image)
     puts "Failure image available: #{image}"
     embed(image, 'image/png')
